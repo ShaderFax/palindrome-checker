40520ca8d402406c7e72d9e8c670312f7b5c45f0
shaderfax <westleyhowe@hotmail.com>
2024-02-06 01:37:13 -0600

    Working on certificate project part 1

---
+++ script.js
@@ -0,0 +1,11 @@
+const getText = document.getElementById("text-input");
+const getButton = document.getElementById("check-btn");
+
+function checkPalindrome() {
+  if(getText.length = 0) {
+    alert("Please input a value");
+  } 
+  else if(getText.length >= 1){
+    
+  }
+}
