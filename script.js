1703dcbad306b9f39ddc05bd28212cd72d9e2439
shaderfax <westleyhowe@hotmail.com>
2024-02-07 02:28:24 -0600

    Added a bit more html/css, still working on js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
-
-function checkPalindrome() {
-    alertMsg();
-}
+str = textInput.value;
+const regex = [A-Za-z0-9]/g;
+const lowerCase = str.toLowerCase().replace(regex, '');
+const reverseString = lowerCase.split('').reverse().join('');
 
 function alertMsg() {
     window.alert("Please input a value");
+}
+
+function checkPalindrome(str) {
+alertMsg();
+    
+    
 }
 
 checkBtn.addEventListener("click", checkPalindrome());
