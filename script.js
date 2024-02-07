d3b583ff967a03abc417b264685c1ecd1acfb374
shaderfax <westleyhowe@hotmail.com>
2024-02-06 23:39:17 -0600

    js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const getText = document.getElementById('text-input');
 const getButton = document.getElementById('check-btn');
 const getResult = document.getElementById('result');
 
-function checkPalindrome(str) {
-    if(getText.length === 0) {
-        window.alert("Please input a value");
-    }
+function checkPalindrome() {
+    
 }
 
 getButton.addEventListener("click", checkPalindrome());
